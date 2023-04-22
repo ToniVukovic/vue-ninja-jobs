aa2ada03abe62bafa0c8e4ec8aba1a8afdaa77e0
Toni Vukovic <tonivukovic1204@gmail.com>
2023-04-22 16:57:34 +0200

    initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. A cupiditate
+      unde officia sed id! Suscipit, saepe rerum itaque eveniet corrupti
+      quibusdam placeat ad officia qui necessitatibus dolor? Quaerat nam
+      voluptatum voluptatem vel, hic cumque ab deserunt officia quibusdam
+      molestias odio, dignissimos error commodi et ea maxime eum quidem.
+      Deleniti iure error quam iusto, animi voluptas quas dolorum voluptates.
+      Ipsam perferendis atque est exercitationem quibusdam facilis obcaecati
+      consequuntur. Enim ducimus officia asperiores repellat voluptatem libero
+      voluptates dignissimos adipisci nulla velit porro fugiat cum error dolor
+      quis facilis illum delectus aspernatur tempora quibusdam, saepe eum sunt.
+      Totam qui consequatur quia iste ea!
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. A cupiditate
+      unde officia sed id! Suscipit, saepe rerum itaque eveniet corrupti
+      quibusdam placeat ad officia qui necessitatibus dolor? Quaerat nam
+      voluptatum voluptatem vel, hic cumque ab deserunt officia quibusdam
+      molestias odio, dignissimos error commodi et ea maxime eum quidem.
+      Deleniti iure error quam iusto, animi voluptas quas dolorum voluptates.
+      Ipsam perferendis atque est exercitationem quibusdam facilis obcaecati
+      consequuntur. Enim ducimus officia asperiores repellat voluptatem libero
+      voluptates dignissimos adipisci nulla velit porro fugiat cum error dolor
+      quis facilis illum delectus aspernatur tempora quibusdam, saepe eum sunt.
+      Totam qui consequatur quia iste ea!
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
---
+++ src/views/jobs/jobDetails.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Jobs detail page</h1>
+    <p>the job id is {{ id }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id"],
+  // data() {
+  //   return {
+  //     id: this.$route.params.id,
+  //   };
+  // },
+};
+</script>
+
+<style></style>
